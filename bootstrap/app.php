8a67398a76b7f369b192f311d794a43929b6f3bf
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-07-06 02:28:27 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // إضافة وسيط CORS لجميع مسارات api
+        $middleware->api(append: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
