af7ca16bc300514349e7aa84b12b7b43f82dcfe2
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-07-06 10:32:34 +0300

    Update Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'car_id',
+        'renter_id',
+        'start_date',
+        'end_date',
+        'total_price',
+        'status',
+        'delivery_method',
+        'delivery_location',
+    ];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function renter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'renter_id');
+    }
 }
