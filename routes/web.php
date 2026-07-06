0f2bf337220848e7594cb1c4f62f970bfaa0197d
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-07-06 10:31:15 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
-    return view('welcome');
+    // تعديل: عرض ملف index.html من مجلد public
+    if (File::exists(public_path('index.html'))) {
+        return File::get(public_path('index.html'));
+    } else {
+        return response('الصفحة الرئيسية غير موجودة', 404);
+    }
 });
