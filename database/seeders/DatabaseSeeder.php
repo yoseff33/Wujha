3b6e0fe2bb7788764c91722236b4c4e3e086ff9d
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-07-06 17:44:01 +0000

    add admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,16 +10,14 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'ادمن وجهة',
+            'phone' => '0500000000',
+            'password' => '12345678',
+            'role' => 'admin',
+            'status' => 'approved',
         ]);
     }
 }
