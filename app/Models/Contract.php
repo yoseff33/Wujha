993052d491b5ec850ec1e4f638bc3efa5e3d5cd4
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-07-06 10:32:52 +0300

    Update Contract.php

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contract extends Model
 {
-    //
+    protected $fillable = [
+        'booking_id',
+        'contract_number',
+        'pdf_link',
+        'qr_code',
+        'status',
+        'start_date',
+        'end_date',
+    ];
+
+    public function booking(): BelongsTo
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
